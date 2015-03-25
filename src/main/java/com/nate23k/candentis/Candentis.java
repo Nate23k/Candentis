a1fc6f6383d5c008b89a5c06db3953b14aa2c1c8
Nate23k <nate23k@hotmail.com>
2015-03-24 21:52:38 -0500

    Loaded into Minecraft
    successfully loaded into minecraft using FML

---
+++ src/main/java/com/nate23k/candentis/Candentis.java
@@ -0,0 +1,35 @@
+package com.nate23k.candentis;
+
+/**
+ * Created on 3/24/2015.
+ */
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="Candentis", name="Candentis", version="1.7.10-1.0")
+public class Candentis
+{
+    @Mod.Instance("Candentis")
+    public static Candentis instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
